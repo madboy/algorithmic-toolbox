cfdd2d3989b196f3153ff61457c44ea0c8d5206c
Kristofer Lindberg <krl@spotify.com>
2017-02-28 20:41:21 +0100

    the segments are more covered

--- src/main/java/covering_segments/CoveringSegments.java
+++ src/main/java/covering_segments/CoveringSegments.java
@@ -4,27 +4,8 @@ import java.util.*;
 
 public class CoveringSegments {
 
-  public static int[] optimalPoints(Segment[] segments) {
-    // sort the segments based on the start point
-    // pick the leftmost one
-    // add new segments as long as their start is within the end
-    // of the currently picked segment
-    // set the point to the furthest segment start
-    // if you reach a segment that starts after end pick that as the new segment
+  public static class Segment implements Comparable<Segment> {
 
-    System.out.println(Arrays.toString(segments));
-    Arrays.sort(segments);
-    System.out.println(Arrays.toString(segments));
-
-    int[] points = new int[2 * segments.length];
-    for (int i = 0; i < segments.length; i++) {
-      points[2 * i] = segments[i].start;
-      points[2 * i + 1] = segments[i].end;
-    }
-    return points;
-  }
-
-  public static class Segment implements Comparable<Segment>{
     int start, end;
 
     Segment(int start, int end) {
@@ -45,6 +26,50 @@ public class CoveringSegments {
       return 1;
     }
   }
+
+  public static int[] optimalPoints(Segment[] segments) {
+    // Boundaries:
+    // 1 ≤ n ≤ 100; 0 ≤ ai ≤ bi ≤ 1000000000
+    // sort the segments based on the start point
+    // pick the leftmost one
+    // add new segments as long as their start is within the end
+    // of the currently picked segment
+    // set the point to the furthest segment start
+    // if you reach a segment that starts after end pick that as the new segment
+    // is start < end always?
+    Arrays.sort(segments);
+    int[] points = new int[2 * segments.length];
+    int point = 0;
+    int currentEnd = 0;
+
+    for (int i = 0; i < segments.length; i++) {
+      // This could be brought outside of the loop
+      // but for now it helps with the thinking
+      if (i == 0) {
+        currentEnd = segments[i].end;
+        points[point] = segments[i].end;
+        point++;
+      } else {
+        if (segments[i].start <= currentEnd) {
+          // This segment can be skipped;
+          // but we should update the end if it's to the left of the currentEnd
+          // And since we're past the first point is should be safe to update
+          // the position of the previous point.
+          if (segments[i].end < currentEnd) {
+            points[point-1] = segments[i].end;
+            currentEnd = segments[i].end;
+          }
+        } else {
+          points[point] = segments[i].end;
+          currentEnd = segments[i].end;
+          point++;
+        }
+      }
+    }
+    points = Arrays.copyOf(points, point);
+    return points;
+  }
+
 
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
--- src/test/java/covering_segments/CoveringSegmentsTest.java
+++ src/test/java/covering_segments/CoveringSegmentsTest.java
@@ -5,11 +5,27 @@ import static org.junit.Assert.assertTrue;
 //import java.util.Random;
 import covering_segments.CoveringSegments.Segment;
 import java.util.Arrays;
+import java.util.Random;
 import org.junit.Before;
 import org.junit.Test;
 
 public class CoveringSegmentsTest {
+
   private CoveringSegments cs;
+
+  private static int randInt(int min, int max) {
+    Random r = new Random();
+    return r.nextInt((max - min) + 1) + min;
+  }
+
+  private void fillSegments(Segment[] segments, int min, int max) {
+    for (int i = 0; i < segments.length; i++) {
+      int start = randInt(min, max);
+      int end = randInt(min, max);
+      segments[i] = new Segment(start, end);
+    }
+  }
+
   @Before
   public void init() {
     cs = new CoveringSegments();
@@ -21,6 +37,54 @@ public class CoveringSegmentsTest {
     segments[0] = new Segment(2, 5);
     segments[1] = new Segment(1, 3);
     segments[2] = new Segment(3, 6);
-    System.out.println(Arrays.toString(cs.optimalPoints(segments)));
+    int[] actual = cs.optimalPoints(segments);
+    int[] expected = new int[]{3};
+    assertTrue(String.format("%s != %s", Arrays.toString(expected), Arrays.toString(actual)),
+               Arrays.equals(expected, actual));
+  }
+
+  @Test
+  public void allInTheSamePlace() {
+    Segment[] segments = new Segment[3];
+    segments[0] = new Segment(2, 5);
+    segments[1] = new Segment(2, 5);
+    segments[2] = new Segment(2, 5);
+    int[] actual = cs.optimalPoints(segments);
+    int[] expected = new int[]{5};
+    assertTrue(String.format("%s != %s", Arrays.toString(expected), Arrays.toString(actual)),
+               Arrays.equals(expected, actual));
+  }
+
+  @Test
+  public void multiSegment() {
+    Segment[] segments = new Segment[4];
+    segments[0] = new Segment(4, 7);
+    segments[1] = new Segment(1, 3);
+    segments[2] = new Segment(2, 5);
+    segments[3] = new Segment(5, 6);
+    int[] actual = cs.optimalPoints(segments);
+    int[] expected = new int[]{3, 6};
+    assertTrue(String.format("%s != %s", Arrays.toString(expected), Arrays.toString(actual)),
+               Arrays.equals(expected, actual));
+  }
+
+  @Test
+  public void nonOverlappingSegments() {
+    Segment[] segments = new Segment[4];
+    segments[0] = new Segment(1, 2);
+    segments[1] = new Segment(3, 4);
+    segments[2] = new Segment(5, 6);
+    segments[3] = new Segment(7, 8);
+    int[] actual = cs.optimalPoints(segments);
+    int[] expected = new int[]{2, 4, 6, 8};
+    assertTrue(String.format("%s != %s", Arrays.toString(expected), Arrays.toString(actual)),
+               Arrays.equals(expected, actual));
+  }
+
+  @Test
+  public void maximumValues(){
+    Segment[] segments =  new Segment[100];
+    fillSegments(segments, 0, 1000000000);
+    cs.optimalPoints(segments);
   }
 }
