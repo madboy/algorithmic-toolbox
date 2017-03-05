be790c171fb8d95d2b84caf6538ff55281f80f4a
Kristofer Lindberg <krl@spotify.com>
2017-03-05 21:36:45 +0100

    some soul searching later

--- src/main/java/binary_search/BinarySearch.java
+++ src/main/java/binary_search/BinarySearch.java
@@ -5,67 +5,88 @@ import java.util.*;
 
 public class BinarySearch {
 
-    // Contraints:
-    // 1≤𝑛,𝑘≤10^5;
-    // 1≤𝑎𝑖 ≤10^9 for all 0≤𝑖<𝑛;
-    // 1≤𝑏𝑗 ≤10^9 for all 0≤𝑗<𝑘;
+  // Contraints:
+  // 1≤𝑛,𝑘≤10^5;
+  // 1≤𝑎𝑖 ≤10^9 for all 0≤𝑖<𝑛;
+  // 1≤𝑏𝑗 ≤10^9 for all 0≤𝑗<𝑘;
 
-    public static int binarySearch(int[] a, int x) {
-        int left = 0, right = a.length;
-        //write your code here
+  public static int binarySearch(int[] a, int x) {
+    // get upper  and lower bound for the array that we're going to search
+    // get the mid point and use that as comparison
+    // if the number we're looking for is mid we're done
+    // else
+    // if it's smaller we should continue looking in the lower part of the array
+    // if it's bigger we'll look in the upper part
 
-        return -1;
+    int left = 0, right = a.length;
+    int i = 0;
+
+    while ((right - left) > 0) {
+      int mid = (right - left) / 2 + left;
+      if (a[mid] == x) {
+        return mid;
+      } else if (x < a[mid]) {
+        right = mid - 1;
+      } else if (a[mid] < x) {
+        left = mid + 1;
+      }
+    }
+    return -1;
+  }
+
+  public static int linearSearch(int[] a, int x) {
+    for (int i = 0; i < a.length; i++) {
+      if (a[i] == x) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  public static void main(String[] args) {
+    FastScanner scanner = new FastScanner(System.in);
+    int n = scanner.nextInt();
+    int[] a = new int[n];
+    for (int i = 0; i < n; i++) {
+      a[i] = scanner.nextInt();
+    }
+    int m = scanner.nextInt();
+    int[] b = new int[m];
+    for (int i = 0; i < m; i++) {
+      b[i] = scanner.nextInt();
+    }
+    for (int i = 0; i < m; i++) {
+      //replace with the call to binarySearch when implemented
+      System.out.print(binarySearch(a, b[i]) + " ");
+    }
+  }
+
+  static class FastScanner {
+
+    BufferedReader br;
+    StringTokenizer st;
+
+    FastScanner(InputStream stream) {
+      try {
+        br = new BufferedReader(new InputStreamReader(stream));
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
     }
 
-    public static int linearSearch(int[] a, int x) {
-        for (int i = 0; i < a.length; i++) {
-            if (a[i] == x) return i;
+    String next() {
+      while (st == null || !st.hasMoreTokens()) {
+        try {
+          st = new StringTokenizer(br.readLine());
+        } catch (IOException e) {
+          e.printStackTrace();
         }
-        return -1;
+      }
+      return st.nextToken();
     }
 
-    public static void main(String[] args) {
-        FastScanner scanner = new FastScanner(System.in);
-        int n = scanner.nextInt();
-        int[] a = new int[n];
-        for (int i = 0; i < n; i++) {
-            a[i] = scanner.nextInt();
-        }
-        int m = scanner.nextInt();
-        int[] b = new int[m];
-        for (int i = 0; i < m; i++) {
-          b[i] = scanner.nextInt();
-        }
-        for (int i = 0; i < m; i++) {
-            //replace with the call to binarySearch when implemented
-            System.out.print(binarySearch(a, b[i]) + " ");
-        }
+    int nextInt() {
+      return Integer.parseInt(next());
     }
-    static class FastScanner {
-        BufferedReader br;
-        StringTokenizer st;
-
-        FastScanner(InputStream stream) {
-            try {
-                br = new BufferedReader(new InputStreamReader(stream));
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
-        String next() {
-            while (st == null || !st.hasMoreTokens()) {
-                try {
-                    st = new StringTokenizer(br.readLine());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            return st.nextToken();
-        }
-
-        int nextInt() {
-            return Integer.parseInt(next());
-        }
-    }
+  }
 }
--- src/test/java/binary_search/BinarySearchTest.java
+++ src/test/java/binary_search/BinarySearchTest.java
@@ -1,12 +1,15 @@
 package binary_search;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+import java.util.Arrays;
 import java.util.Random;
 import org.junit.Before;
 import org.junit.Test;
 
 public class BinarySearchTest {
+
   private BinarySearch bs;
 
   private static int randInt(int min, int max) {
@@ -20,15 +23,49 @@ public class BinarySearchTest {
   }
 
   @Test
-  public void shortList() {
-    int[] l = new int[] {1, 5, 8, 12, 13};
-    int[] s = new int[] {8, 1, 23, 1, 11};
-    int[] expected = new int[] {2, 0, -1, 0, -1,};
+  public void middleNumber() {
+    int[] l = new int[]{1, 2, 3, 4, 5};
+    assertEquals(2, bs.binarySearch(l, 3));
+  }
 
-    for (int i = 0; i < s.length; i++){
+  @Test
+  public void lowerNumber() {
+    int[] l = new int[]{1, 2, 3, 4, 5};
+    assertEquals(0, bs.binarySearch(l, 1));
+  }
+
+  @Test
+  public void upperNumber(){
+    int[] l = new int[]{1,2,3,4,5};
+    assertEquals(4, bs.binarySearch(l, 5));
+  }
+
+  @Test
+  public void duplicates(){
+    int[] l = new int[]{1,2,2,3,3,4,5};
+    assertEquals(1, bs.binarySearch(l, 2));
+  }
+
+  @Test
+  public void shortList() {
+    int[] l = new int[]{1, 5, 8, 12, 13};
+    int[] s = new int[]{8, 1, 23, 1, 11};
+    int[] expected = new int[]{2, 0, -1, 0, -1,};
+
+    for (int i = 0; i < s.length; i++) {
       int actual = bs.binarySearch(l, s[i]);
       assertEquals(expected[i], actual);
     }
+  }
+
+  @Test
+  public void longList() {
+    int[] l = new int[100000];
+    for (int i = 0; i < l.length; i++){
+      l[i] = randInt(1, 1000000000);
+    }
+    Arrays.sort(l);
+    bs.binarySearch(l, 912090);
   }
 
 }
